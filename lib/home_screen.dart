c90b449c59f4003c4b574173df16127e35763fb8
Aagash21 <secondemailaagash@gmail.com>
2024-09-30 17:26:05 +0100

    Upload functionality setup

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,26 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  File? _image;
+
+  Future<void> _uploadImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await showDialog<XFile?>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Select Image Source'),
+          actions: [
+            TextButton(
+              onPressed: () async {
+                final file = await picker.pickImage(source: ImageSource.camera);
+                Navigator.pop(context, file);
+              },
+              child: Text('Camera'),
+            ),
+            TextButton(
+              onPressed: () async {
+                final file = await picker.pickImage(source: ImageSource.gallery);
+                Navigator.pop(context, file);
+              },
+              child: Text('Gallery'),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text('Home Screen'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Welcome to the Home Screen!',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
+            _image == null
+                ? Text('No image selected.')
+                : Image.file(_image!, height: 200), // Display the selected image
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context); // Navigate back to SignInScreen
-              },
-              child: Text('Log Out'),
+              onPressed: _uploadImage,
+              child: Text('Upload Image'),
             ),
           ],
         ),
