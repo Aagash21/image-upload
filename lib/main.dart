f6a207ffa2990b9266c9cc8c2d6dc970d9b04ac4
Aagash21 <secondemailaagash@gmail.com>
2024-09-30 16:47:23 +0100

    Updated files

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: SignInScreen(),  // Ensure this widget is defined
+      home: SignInScreen(), // This should match your created SignInScreen class
     );
   }
 }
